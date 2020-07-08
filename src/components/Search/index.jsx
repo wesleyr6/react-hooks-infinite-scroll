32b92a102dac959b129bea10b317299d4ed83b23
Wesley Amaro <contato@wesleyamaro.com>
2020-07-08 16:43:02 -0500

    feat: search with queryString

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,7 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import queryString from "query-string";
 import moment from "moment";
 import { useForm, Controller } from "react-hook-form";
+import { withRouter } from "react-router-dom";
 import SearchContext from "../../context/search";
 import { Row, Col } from "../Grid";
 import Paginator from "../Paginator";
@@ -9,22 +11,116 @@ import Button from "../Button";
 import Loader from "../Loader";
 import AlertMessages from "../AlertMessages";
 import Field from "../Field";
+import { useUpdateHistory } from "../../hooks/search";
+import { isNumber, isValidUnixDate } from "../../helpers";
 import styles from "./index.module.sass";
 
-const Search = ({ itemsBeingShowed, children }) => {
-  const { handleSubmit, register, errors, control, watch } = useForm({
+const Search = ({ children, history }) => {
+  const [setUpdateHistory] = useUpdateHistory(history);
+
+  const {
+    handleSubmit,
+    register,
+    errors,
+    control,
+    watch,
+    getValues,
+    setValue,
+  } = useForm({
     validateCriteriaMode: "all",
   });
 
-  const { searchLoading, searchResults, searchError } = useContext(
+  const { searchLoading, searchResults, searchError, fetchSearch } = useContext(
     SearchContext
   );
+
+  const [defaultSearch, setDefaultSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
 
   const watchDateFrom = watch("txtDateFrom");
   const watchDateTo = watch("txtDateTo");
 
+  useEffect(() => {
+    if (!history.location.search) {
+      updateSearchHistory(getValues());
+    } else {
+      fetchResults();
+    }
+
+    // eslint-disable-next-line
+  }, [history.location.search]);
+
+  const onHandlePageChange = async (page) => {
+    setUpdateHistory([{ name: "page", value: page }]);
+  };
+
+  const updateSearchHistory = (fields) => {
+    const newFields = fields;
+
+    if (fields.txtDateFrom) {
+      newFields.txtDateFrom = moment(fields.txtDateFrom).unix();
+    }
+
+    if (fields.txtDateTo) {
+      newFields.txtDateTo = moment(fields.txtDateTo).unix();
+    }
+
+    const historyFields = [
+      { name: "search", value: newFields.txtSearch },
+      { name: "dateFrom", value: newFields.txtDateFrom },
+      { name: "dateTo", value: newFields.txtDateTo },
+      { name: "page", value: 1 },
+    ];
+
+    setUpdateHistory(historyFields);
+  };
+
   const onHandleSubmit = (fields) => {
-    console.log(fields);
+    updateSearchHistory(fields);
+  };
+
+  const fetchResults = () => {
+    const { dateFrom, dateTo, search, page } =
+      queryString.parse(history.location.search) || "";
+
+    if (dateFrom && isValidUnixDate(dateFrom)) {
+      const formatDate = moment.unix(dateFrom).format();
+
+      if (formatDate) {
+        setValue("txtDateFrom", new Date(formatDate));
+      }
+    }
+
+    if (dateTo && isValidUnixDate(dateTo)) {
+      const formatDate = moment.unix(dateTo).format();
+
+      if (formatDate) {
+        setValue("txtDateTo", new Date(formatDate));
+      }
+    }
+
+    if (search) {
+      setDefaultSearch(search);
+    }
+
+    const newFields = getValues();
+
+    if (page && isNumber(page)) {
+      setCurrentPage(Number(page));
+      newFields.page = Number(page);
+    } else {
+      newFields.page = currentPage;
+    }
+
+    if (newFields.txtDateFrom) {
+      newFields.txtDateFrom = moment(newFields.txtDateFrom).unix();
+    }
+
+    if (newFields.txtDateTo) {
+      newFields.txtDateTo = moment(newFields.txtDateTo).unix();
+    }
+
+    fetchSearch(newFields);
   };
 
   return (
@@ -40,6 +136,7 @@ const Search = ({ itemsBeingShowed, children }) => {
             name="txtSearch"
             id="txtSearch"
             placeholder="search case descriptions"
+            defaultValue={defaultSearch}
             inputErrors={errors}
             ref={register}
           />
@@ -95,7 +192,12 @@ const Search = ({ itemsBeingShowed, children }) => {
         </Col>
 
         <Col lg={2} md={2} sm={12} xs={12}>
-          <Button type="submit" className={styles.btn} text="find cases" />
+          <Button
+            type="submit"
+            className={styles.btn}
+            text="find cases"
+            disabled={searchLoading}
+          />
         </Col>
       </Row>
 
@@ -105,9 +207,9 @@ const Search = ({ itemsBeingShowed, children }) => {
         message={searchError}
       />
 
-      {searchLoading ? (
-        <Loader />
-      ) : (
+      {searchLoading && <Loader />}
+
+      {!searchLoading && !searchError && (
         <>
           {searchResults.length > 0 ? (
             <div className={styles.totalItems}>
@@ -126,9 +228,9 @@ const Search = ({ itemsBeingShowed, children }) => {
       {!searchLoading && searchResults.length > 0 && (
         <Paginator
           itemsBeingShowed={searchResults.length}
-          currentPage={1}
+          currentPage={currentPage}
           totalItems={300}
-          onHandlePageChange={(showing, page) => {}}
+          onHandlePageChange={(showing, page) => onHandlePageChange(page)}
         />
       )}
     </form>
@@ -143,4 +245,4 @@ Search.defaultProps = {
   children: [],
 };
 
-export default Search;
+export default withRouter(Search);
--- src/context/search.js
+++ src/context/search.js
@@ -1,24 +1,46 @@
 import React, { createContext, useState } from "react";
-import JSON_searchResults from "../mocks/searchResuts.json";
+import { fetchSearchAction } from "../actions/stolen_bikes";
+
 const SearchContext = createContext();
 
-export const SearchProvider = ({ children }) => {
+export const SearchProvider = (props) => {
   const [searchLoading, setSearchLoading] = useState(false);
-  const [searchResults, setSearchResults] = useState(JSON_searchResults.data);
+  const [searchResults, setSearchResults] = useState([]);
   const [searchError, setSearchError] = useState("");
+  const itemsPerPage = 10;
+
+  const fetchSearch = async (fields) => {
+    setSearchLoading(true);
+
+    const newFields = fields;
+
+    try {
+      const { incidents } = await fetchSearchAction({
+        occurred_before: newFields.txtDateTo,
+        occurred_after: newFields.txtDateFrom,
+        page: newFields.page || 1,
+        per_page: itemsPerPage,
+        incident_type: "theft",
+        query: newFields.txtSearch,
+      });
+      setSearchResults(incidents);
+    } catch (err) {
+      setSearchError(err);
+    }
+
+    setSearchLoading(false);
+  };
 
   return (
     <SearchContext.Provider
       value={{
         searchLoading,
-        setSearchLoading,
         searchResults,
-        setSearchResults,
         searchError,
-        setSearchError,
+        fetchSearch,
       }}
     >
-      {children}
+      {props.children}
     </SearchContext.Provider>
   );
 };
