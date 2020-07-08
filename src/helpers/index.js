e1573b4f24385d2abb9984c2a18d586eb461b9bb
Wesley Amaro <contato@wesleyamaro.com>
2020-07-08 16:42:30 -0500

    feat: helpers

---
+++ src/helpers/index.js
@@ -0,0 +1,7 @@
+export const isNumber = (_string) => /^[0-9]*$/.test(_string);
+
+export const isDate = (_date) => _date instanceof Date && !isNaN(_date);
+
+export const isValidUnixDate = (_date) => {
+  return isNumber(_date) && !isNaN(new Date(_date).getTime());
+};
