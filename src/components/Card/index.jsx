00a40f27779ace41857ed52af31bced6a3d9b0a2
Wesley Amaro <contato@wesleyamaro.com>
2020-07-08 12:43:13 -0500

    feat: lazy load

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import moment from "moment";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Link } from "react-router-dom";
 import IMG_NoPhoto from "../../assets/images/no-image.svg";
 import styles from "./index.module.sass";
@@ -16,43 +17,46 @@ const Card = ({
 }) => {
   return (
     <li className={styles.card}>
-      <Link to={`/${id}`}>
-        <img
+      <Link to={`/case/${id}`} className={styles.thumbnailLink}>
+        <LazyLoadImage
           src={thumbnail ? thumbnail : IMG_NoPhoto}
           alt={title}
+          effect="blur"
           className={styles.thumbnail}
         />
-
-        <div className={styles.content}>
-          <span className={styles.title}>{title}</span>
-
-          {occurred_at && (
-            <span className={styles.description}>
-              <strong>Ocurred at:</strong>{" "}
-              {moment.unix(occurred_at).format("dddd, MMMM Do YYYY, h:mm:ss a")}
-            </span>
-          )}
-
-          {updated_at && (
-            <span className={styles.description}>
-              <strong>Updated at:</strong>{" "}
-              {moment.unix(updated_at).format("dddd, MMMM Do YYYY, h:mm:ss a")}
-            </span>
-          )}
-
-          {address && (
-            <span className={styles.description}>
-              <strong>Location:</strong> {address}
-            </span>
-          )}
-
-          {description && (
-            <span className={styles.description}>
-              <strong>Description:</strong> {description}
-            </span>
-          )}
-        </div>
       </Link>
+
+      <div className={styles.content}>
+        <Link to={`/case/${id}`}>
+          <span className={styles.title}>{title}</span>
+        </Link>
+
+        {occurred_at && (
+          <span className={styles.description}>
+            <strong>Ocurred at:</strong>{" "}
+            {moment.unix(occurred_at).format("dddd, MMMM Do YYYY, h:mm:ss a")}
+          </span>
+        )}
+
+        {updated_at && (
+          <span className={styles.description}>
+            <strong>Updated at:</strong>{" "}
+            {moment.unix(updated_at).format("dddd, MMMM Do YYYY, h:mm:ss a")}
+          </span>
+        )}
+
+        {address && (
+          <span className={styles.description}>
+            <strong>Location:</strong> {address}
+          </span>
+        )}
+
+        {description && (
+          <span className={styles.description}>
+            <strong>Description:</strong> {description}
+          </span>
+        )}
+      </div>
     </li>
   );
 };
