044c8ff02bcd87254aedabda4d79c921b2614e10
Wesley Amaro <contato@wesleyamaro.com>
2020-07-08 12:45:14 -0500

    styles

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -4,15 +4,10 @@ import MasterPage from "../../components/MasterPage";
 import Wrapper from "../../components/Wrapper";
 import Search from "../../components/Search";
 import SearchContext from "../../context/search";
-import Loader from "../../components/Loader";
-import AlertMessages from "../../components/AlertMessages";
 import ListOfItems from "../../components/ListOfItems";
-import "./index.sass";
 
 const Home = () => {
-  const { searchLoading, searchResults, searchError } = useContext(
-    SearchContext
-  );
+  const { searchLoading, searchResults } = useContext(SearchContext);
 
   return (
     <>
@@ -20,19 +15,11 @@ const Home = () => {
 
       <MasterPage>
         <Wrapper>
-          <Search />
-
-          <AlertMessages
-            show={!searchLoading && !!searchError}
-            type="error"
-            message={searchError}
-          />
-
-          {searchLoading && <Loader />}
-
-          {!searchLoading && searchResults.length > 0 && (
-            <ListOfItems items={searchResults} />
-          )}
+          <Search>
+            {!searchLoading && searchResults.length > 0 && (
+              <ListOfItems items={searchResults} />
+            )}
+          </Search>
         </Wrapper>
       </MasterPage>
     </>
