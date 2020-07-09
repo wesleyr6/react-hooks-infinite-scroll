c7fc71ad3e31a7d04b4f60f4abd86942db25a9bc
Wesley Amaro <contato@wesleyamaro.com>
2020-07-09 11:01:29 -0500

    feat: cleaning code

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-const path = require("path");
 const express = require("express");
 const secure = require("ssl-express-www");
-const expressStaticGzip = require("compression");
+const compression = require("compression");
 
 const port = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.static(`${__dirname}/build`));
 app.use(secure);
-
-app.get(
-  "*",
-  expressStaticGzip(path.join(__dirname), {
-    urlContains: "static/",
-    fallthrough: false,
-    enableBrotli: true,
-  })
-);
+app.use(compression());
 
 app.listen(port, () => {
   console.log(`Application is running...`);
