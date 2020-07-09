8717420d9146d333a8b16cf0b43eb7434a7aa7ea
Wesley Amaro <contato@wesleyamaro.com>
2020-07-09 11:08:50 -0500

    fix: re-ordering

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const compression = require("compression");
 const port = process.env.PORT || 4000;
 const app = express();
 
-app.use(express.static(`${__dirname}/build`));
 app.use(secure);
 app.use(compression());
+app.use(express.static(`${__dirname}/build`));
 
 app.listen(port, () => {
   console.log(`Application is running...`);
