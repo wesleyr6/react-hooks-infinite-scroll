3031342226d832174502a36f40dd224192b1b67c
Wesley Amaro <contato@wesleyamaro.com>
2020-07-08 22:16:38 -0500

    feat: register serviceworker

--- src/index.js
+++ src/index.js
@@ -9,8 +9,4 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 import "react-datepicker/dist/react-datepicker.css";
 
 ReactDOM.render(<Routers />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
