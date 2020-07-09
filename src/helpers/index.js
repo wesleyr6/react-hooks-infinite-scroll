5c9ee97a16194ffcc6934844e2be4846b32c4704
Wesley Amaro <contato@wesleyamaro.com>
2020-07-08 20:35:00 -0500

    fix: isValidUnixDate

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,5 +3,5 @@ export const isNumber = (_string) => /^[0-9]*$/.test(_string);
 export const isDate = (_date) => _date instanceof Date && !isNaN(_date);
 
 export const isValidUnixDate = (_date) => {
-  return isNumber(_date) && !isNaN(new Date(_date).getTime());
+  return isNumber(_date) && !isNaN(new Date(Number(_date)).getTime());
 };
