56fff9ec06a70952face08ad5f6a581f7c7508ce
Wesley Amaro <contato@wesleyamaro.com>
2020-07-08 22:16:08 -0500

    feat: ssl-express-www

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const path = require("path");
 const express = require("express");
+const secure = require("ssl-express-www");
 
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.static(`${__dirname}/build`));
+
+app.use(secure);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
